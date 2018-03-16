3d728307a8d1c775613c7f3496fd7befd15a729f
Carlos Melo <dgtlab@hotmail.com>
2018-03-15 23:08:10 -0300

    Estudo 3 aulas botstrap 3

--- bstrap/gulpfile.js
+++ bstrap/gulpfile.js
@@ -3,4 +3,5 @@ gulp.task("copy",
 function()
 {
     gulp.src("./bower_components/bootstrap/dist/**").pipe(gulp.dest("./wwwroot/lib/bootstrap"));
+    gulp.src("./bower_components/jquery/dist/**").pipe(gulp.dest("./wwwroot/lib/jquery"));
 });
