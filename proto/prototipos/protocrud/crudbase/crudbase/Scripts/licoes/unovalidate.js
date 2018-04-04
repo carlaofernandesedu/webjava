70031e71a4a907fb85dc1e16cb8ec11a852cb189
Carlos Melo <dgtlab@hotmail.com>
2018-04-04 02:18:39 -0300

    Tratamento response

--- proto/prototipos/protocrud/crudbase/crudbase/Scripts/licoes/unovalidate.js
+++ proto/prototipos/protocrud/crudbase/crudbase/Scripts/licoes/unovalidate.js
@@ -1,6 +1,29 @@
-﻿$("form").submit(function (e) {
+﻿$("#formcui").submit(function (e) {
     if ($(this).valid()) {
+        $.post('/Company/CreateUserInformation', $(this).serialize(), function (data) {
+            $("#dadosdinamicos").html(data);
+            $.validator.unobtrusive.parse($("#dadosdinamicos"));
+        });
 
     }
     e.preventDefault();
+    return false;
 });
+$("#prevButton").click(function () {
+    $.post('/Company/CreateUserPrevious', $($("form")[0]).serialize(), function (data) {
+        $("#dadosdinamicos").html(data);
+        $.validator.unobtrusive.parse($("#dadosdinamicos"));
+    });
+});
+$("#formcei").submit(function (e) {
+    if ($(this).valid()) {
+        $.post('/Company/CreateUserCompanyInformation', $(this).serialize(), function (data) {
+            $("#dadosdinamicos").html(data);
+            $.validator.unobtrusive.parse($("#dadosdinamicos"));
+        });
+    }
+    e.preventDefault();
+    return false;
+});
+
+//http://nadeemkhedr.com/how-the-unobtrusive-jquery-validate-plugin-works-internally-in-asp-net-mvc/
