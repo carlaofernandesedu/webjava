d300b83399bc283f9835b4623b6053691db56a90
cmelo <cmelo@D-BOA-BP137162>
2018-04-19 14:41:39 -0300

    Inclusao do Editor versao atual sistemaintegrado

---
+++ proto/prototipos/protocrud/crudbase/crudbase/Scripts/licoes/validaEditor.js
@@ -0,0 +1,83 @@
+﻿'use strict';
+var VALIDAEDITOR = (function () {
+
+    function init()
+    {
+        bindAll();
+    }
+
+    function bindAll()
+    {
+            bindEditores();
+            bindBotoes();
+    }
+
+    function bindEditores()
+    {
+        $("#txtEditor").Editor();
+        $("#txtEditor2").Editor();
+    }
+
+    function bindBotoes() {
+        $("#adicionareditor").on("click", function () {
+            let texto = $("#origem").val();
+            let estaSelecionado = false;
+            var object;
+            if (window.getSelection) {
+                object = window.getSelection();
+                let classeEditor = object.anchorNode.parentElement.className;
+                if (classeEditor == "row-fluid Editor-container" || classeEditor == "Editor-editor"
+                    || object.anchorNode.parentElement == "div.Editor-editor" || classeEditor=="manualadd-editor") {
+                    estaSelecionado = true;
+                }
+            }
+            if (estaSelecionado) {
+                $("#txtEditor").Editor("insertTextAtSelection", texto);
+            }
+            else {
+                let textoeditor = $("#txtEditor").Editor("getText");
+                //let linha = (textoeditor != "") ? "<br>" : "";
+                texto = textoeditor + "<div class='manualadd-editor'>" + texto + "</div>";
+                $("#txtEditor").Editor("setText", texto);
+            }
+        });
+        $("#adicionareditor2").on("click", function () {
+            let texto = $("#origem").val();
+            let estaSelecionado = false;
+            var object;
+            if (window.getSelection) {
+                object = window.getSelection();
+                let classeEditor = object.anchorNode.parentElement.className;
+                if (classeEditor == "row-fluid Editor-container" || classeEditor == "Editor-editor"
+                    || object.anchorNode.parentElement == "div.Editor-editor" || classeEditor=="manualadd-editor") {
+                    estaSelecionado = true;
+                }
+            }
+            if (estaSelecionado) {
+                $("#txtEditor2").Editor("insertTextAtSelection", texto);
+            }
+            else {
+                let textoeditor = $("#txtEditor2").Editor("getText");
+                //let linha = (textoeditor != "") ? "<br>" : "";
+                texto = textoeditor + "<div class='manualadd-editor'>" + texto + "</div>";
+                $("#txtEditor2").Editor("setText", texto);
+            }
+        });
+        $("#contadoreditor").on("click", function () {
+            let caracteres = $("#txtEditor").Editor("getCharCount");
+            alert(caracteres);
+        });
+        $("#contadoreditor2").on("click", function () {
+            let caracteres = $("#txtEditor2").Editor("getCharCount");
+            alert(caracteres);
+        });
+    }
+
+    return {
+        Init:init
+    }
+
+}());
+$(function () {
+    VALIDAEDITOR.Init();
+});
---
+++ proto/prototipos/protocrud/crudbase/crudbase/Scripts/licoes/validaEditorSI.js
@@ -0,0 +1,98 @@
+﻿'use strict';
+var VALIDAEDITOR = (function () {
+
+    function init()
+    {
+        bindAll();
+    }
+
+    function bindAll()
+    {
+            bindEditores();
+            bindBotoes();
+    }
+
+    function bindEditores()
+    {
+        $('[name="Resposta"]').Editor({
+            "insert_table": false,
+            "fonts": false,
+            "styles": false,
+            "font_size": false,
+            "color": false,
+            "ol": false,
+            "ul": false,
+            "indent": false,
+            "outdent": false,
+            "insert_img": false,
+            "insert_link": false,
+            "unlink": false,
+            "strikeout": false,
+            "hr_line": false,
+            "print": false,
+            "select_all": false,
+            "togglescreen": false,
+            "splchars": false,
+            "block_quote": false,
+            "source": false
+        });
+    }
+
+    function bindBotoes() {
+        $("#habilitarcheckbox").off("click");
+        $("#habilitarcheckbox").on("click", function(){
+            $('input[type="checkbox"]').prop('disabled', false);
+        }
+         );
+
+        $("#confirmarRespostaPadrao").off("click");
+        $("#confirmarRespostaPadrao").on("click", function () {
+            versaoAtual();
+            //let texto = $("#origem").val();
+            //let estaSelecionado = false;
+            //var object;
+            //if (window.getSelection) {
+            //    object = window.getSelection();
+            //    let classeEditor = object.anchorNode.parentElement.className;
+            //    if (classeEditor == "row-fluid Editor-container" || classeEditor == "Editor-editor"
+            //        || object.anchorNode.parentElement == "div.Editor-editor" || classeEditor=="manualadd-editor") {
+            //        estaSelecionado = true;
+            //    }
+            //}
+            //if (estaSelecionado) {
+            //    $("#txtEditor").Editor("insertTextAtSelection", texto);
+            //}
+            //else {
+            //    let textoeditor = $("#txtEditor").Editor("getText");
+            //    //let linha = (textoeditor != "") ? "<br>" : "";
+            //    texto = textoeditor + "<div class='manualadd-editor'>" + texto + "</div>";
+            //    $("#txtEditor").Editor("setText", texto);
+            //}
+        });
+    }
+
+    function versaoAtual()
+    {
+        var txtaResposta = $(" .Editor-editor #Editor-editor");
+        txtaResposta.focus();
+        if (txtaResposta.length === 0) {
+            txtaResposta = $(" .Editor-editor");
+        }
+        var texto = '<p>' + $('textarea[name="Resposta"]').val() + '</p>';
+        var chechtexto = "";
+        //txtaResposta = $(" .Editor-editor");
+        $('input[name=selectedResposta]:checked').each(function () {
+            chechtexto = chechtexto + $(this).closest("div[class=row]").find('#span_descricao').html() + "<br/>";
+            $('input[type="checkbox"]').prop('disabled', true);
+        });
+        txtaResposta.html(texto + '<br>' + chechtexto);
+    }
+
+    return {
+        Init:init
+    }
+
+}());
+$(function () {
+    VALIDAEDITOR.Init();
+});
