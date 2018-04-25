50b84ca4fa4c2b7c6aafddc1984ebf7b6e28df8b
Carlos Melo <dgtlab@hotmail.com>
2018-04-25 00:13:28 -0300

    Subir controle DataTable

---
+++ proto/prototipos/protocrud/crudbase/crudbase/Scripts/licoes/validaDataTables.js
@@ -0,0 +1,23 @@
+﻿var DATATABLESNET = (function () {
+
+    function init() {
+        bindAll();
+    }
+
+    function bindAll() {
+        bindDataTable();
+    }
+
+    function bindDataTable() {
+        $("#tabela01").DataTable();
+    }
+
+    return {
+        Init:init
+    }
+
+}());
+
+$(function () {
+    DATATABLESNET.Init();
+});
