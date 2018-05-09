6aa28af70377c97d966be91037b6f4e3c574adb1
cmelo <cmelo@D-BOA-BP137162>
2018-05-09 15:22:37 -0300

    Tratamento Data Table Fornecedor

---
+++ proto/prototipos/protocrud/crudbase/crudbase/Scripts/licoes/exemploDataTableFornecedor.js
@@ -0,0 +1,32 @@
+﻿var FORNECEDOR = (function () {
+
+    function init() {
+        bindAll();
+
+        CONTROLES.Tabela.Configurar();
+    }
+    function bindAll() {
+        bindFiltrarPaginado();
+    }
+
+    function bindFiltrarPaginado() {
+        $('.frm-filtro').off('click', '.acoes #btnFiltrarPaginado');
+        $('.frm-filtro').on('click', '.acoes #btnFiltrarPaginado', function () {
+            var tabela = $('table.dataTable').DataTable();
+            tabela.page(1).draw();
+
+            return false;
+        });
+    }
+
+
+    return {
+        Init: function () {
+            init();
+        }
+    };
+}());
+
+$(function () {
+    FORNECEDOR.Init();
+});
