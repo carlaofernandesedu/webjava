2450fc793a6f68ddbce6f8618463988dc7dcdffa
cmelo <cmelo@D-BOA-BP137162>
2018-05-10 13:33:24 -0300

    Informacao Crud Filtro

--- proto/prototipos/protocrud/crudbase/crudbase/Scripts/Base/crudfiltro.js
+++ proto/prototipos/protocrud/crudbase/crudbase/Scripts/Base/crudfiltro.js
@@ -130,7 +130,7 @@
                     else {
                         carregarLista(response);
                         CRUDFILTRO.Evento.PosListar();
-                        CONTROLES.Tabela.Configurar();
+                        //CONTROLES.Tabela.Configurar();
                     }
                 },
                 error: function (xhr) {
